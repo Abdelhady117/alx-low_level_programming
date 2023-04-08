7ce5576a1876628a33d7b2f1762d22acdb51b654
Abdelhady117 <abdelhadymohammed1763@gmail.com>
2023-04-08 08:54:15 -0700

    0x09. C - Static libraries

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+  * main - Enrty point
+  * Return: Always zero
+  */
+int main(void)
+{
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t");
+	return (0);
+}
